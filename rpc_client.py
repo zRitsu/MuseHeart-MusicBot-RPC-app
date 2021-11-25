a711eaced1cc6f6842ca64f91136208d92680dcb
zRitsu <74823568+zRitsu@users.noreply.github.com>
2021-11-24 22:51:02 -0300

    oh sh...

--- rpc_client.py
+++ rpc_client.py
@@ -247,7 +247,7 @@ class RpcTest:
                     state += f'{self.get_lang("playlist")}: {pl_name}'
 
                 elif ab_url:
-                    buttons.append({"label": fix_characters(self.get_lang("album") + ab_name, 30), "url": ab_url.replace("www.", "")})
+                    buttons.append({"label": fix_characters(f'{self.get_lang("album")}: ' + ab_name, 30), "url": ab_url.replace("www.", "")})
 
             if not state:
                 state = "   "
