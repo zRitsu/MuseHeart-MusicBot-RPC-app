d3d5bd5dd1e4d1d14b055f639f0170ddbd9118ba
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-01-04 01:53:24 -0300

    várias correções e melhorias quanto ao suporte a múltiplas instâncias do discord.

--- rpc_client.py
+++ rpc_client.py
@@ -28,7 +28,7 @@ class MyDiscordIPC(DiscordIPC):
 
         self.updating = True
 
-        await asyncio.sleep(15)
+        #await asyncio.sleep(15)
 
         self.updating = False
 
@@ -98,11 +98,11 @@ for i in range(10):
     except:
         continue
 
-    user_id = rpc.data['data']['user']['id']
+    user_id_ = rpc.data['data']['user']['id']
     user = f"{rpc.data['data']['user']['username']}#{rpc.data['data']['user']['discriminator']}"
-    user_clients[int(user_id)] = {"pipe": i, "user": user}
+    user_clients[int(user_id_)] = {"pipe": i, "user": user}
     rpc.user = user
-    print(f"RPC conectado: {user} [{user_id}] pipe: {i}")
+    print(f"RPC conectado: {user} [{user_id_}] pipe: {i}")
 
 
 if not user_clients:
@@ -150,6 +150,8 @@ class RpcClient:
 
     async def update(self, user_id: int, bot_id: int, data: dict):
 
+        data = dict(data)
+
         self.check_presence(user_id, bot_id)
 
         if not data:
@@ -158,7 +160,7 @@ class RpcClient:
 
         payload = {
             "assets": {
-                "large_image": data.pop("thumb", config["assets"]["app"]).replace("mqdefault", "default"),
+                "large_image": data.pop("thumb", config["assets"]["app"]),
                 "small_image": "https://cdn.discordapp.com/attachments/480195401543188483/733507238290915388/cd.gif"
             },
             "timestamps": {}
@@ -235,61 +237,63 @@ class RpcClient:
 
             buttons = []
 
-            if track:
+            #if track:
 
-                if url := track.get("url"):
-                    buttons.append({"label": self.get_lang("listen"), "url": url.replace("www.", "")})
+            if url := track.get("url"):
+                buttons.append({"label": self.get_lang("listen"), "url": url.replace("www.", "")})
 
-                state += f'{self.get_lang("author")}: {track["author"]}'
+            state += f'{self.get_lang("author")}: {track["author"]}'
 
-                pl_url = track.get("playlist_url")
-                pl_name = track.get("playlist_name")
-                ab_url = track.get("album_url")
-                ab_name = track.get("album_name")
+            pl_url = track.get("playlist_url")
+            playlist_name = track.get("playlist_name")
+            ab_url = track.get("album_url")
+            album_name = track.get("album_name")
 
-                if not pl_url:
-                    pl_url = "https://cdn.discordapp.com/attachments/480195401543188483/802406033493852201/unknown.png"
+            if not pl_url:
+                pl_url = "https://cdn.discordapp.com/attachments/480195401543188483/802406033493852201/unknown.png"
 
-                if pl_name and pl_url:
+            if playlist_name and pl_url:
 
-                    if 'youtube.com' in pl_url:
-                        pl_url = "https://www.youtube.com/playlist?list=" + \
-                                 (pl_url.split('?list=' if '?list=' in pl_url else '&list='))[1]
+                if 'youtube.com' in pl_url:
+                    pl_url = "https://www.youtube.com/playlist?list=" + \
+                             (pl_url.split('?list=' if '?list=' in pl_url else '&list='))[1]
 
-                    if (pl_size := len(pl_name)) > 25:
-                        state += f' | {self.get_lang("playlist")}: {pl_name}'
-                        buttons.append({"label": self.get_lang("view_playlist"), "url": pl_url.replace("www.", "")})
+                if (playlist_size := len(playlist_name)) > 25:
+                    state += f' | {self.get_lang("playlist")}: {playlist_name}'
+                    buttons.append({"label": self.get_lang("view_playlist"), "url": pl_url.replace("www.", "")})
 
-                    else:
+                else:
 
-                        if pl_size < 15:
-                            pl_name = f"Playlist: {pl_name}"
+                    if playlist_size < 15:
+                        playlist_name = f"Playlist: {playlist_name}"
 
-                        buttons.append({"label": pl_name, "url": pl_url.replace("www.", "")})
+                    buttons.append({"label": playlist_name, "url": pl_url.replace("www.", "")})
 
-                elif state and pl_name:
-                    state += f' | {pl_name}'
+            elif state and playlist_name:
+                state += f' | {playlist_name}'
 
-                elif pl_name:
-                    state += f'{self.get_lang("playlist")}: {pl_name}'
+            elif playlist_name:
+                state += f'{self.get_lang("playlist")}: {playlist_name}'
 
-                elif ab_url:
+            elif ab_url:
 
-                    if (ab_size := len(ab_name)) > 22:
-                        state += f' | {self.get_lang("album")}: {ab_name}'
-                        buttons.append({"label": self.get_lang("view_album"), "url": ab_url.replace("www.", "")})
+                if (album_size := len(album_name)) > 22:
+                    state += f' | {self.get_lang("album")}: {album_name}'
+                    buttons.append({"label": self.get_lang("view_album"), "url": ab_url.replace("www.", "")})
 
-                    else:
+                else:
 
-                        if ab_size < 17:
-                            ab_name = f"{self.get_lang('album')}: {ab_name}"
+                    if album_size < 17:
+                        album_name = f"{self.get_lang('album')}: {album_name}"
 
-                        buttons.append({"label": ab_name, "url": ab_url})
+                    buttons.append({"label": album_name, "url": ab_url})
 
             if not state:
                 state = "   "
 
             payload['state'] = state
+
+            payload["type"] = 3
 
             if buttons:
                 payload["buttons"] = buttons
@@ -334,7 +338,7 @@ class RpcClient:
                     print(f"Websocket conectado: {uri}")
 
                     for i in user_clients:
-                        await ws.send(json.dumps({"user_id": i}))
+                        await ws.send(json.dumps({"op": "rpc_update", "user_id": i}))
 
                     async for msg in ws:
 
@@ -344,66 +348,86 @@ class RpcClient:
                             traceback.print_exc()
                             continue
 
-                        user_id = data.pop("user", None)
-                        if not user_id:
+                        op = data.pop("op", None)
+
+                        if not op:
                             continue
 
-                        op = data.pop("op")
                         public = data.pop("public", True)
                         bot_id = data.pop("bot_id", None)
-                        user = user_clients[user_id]["user"]
 
-                        users.add(user_id)
                         bots.add(bot_id)
 
-                        print(f"op: {op} | {user} [{user_id}] | bot: {bot_id}")
+                        users_ws = data.pop("users", None)
 
-                        match op:
+                        if not users_ws:
+                            continue
 
-                            case "update":
+                        else:
+                            users_ws = [u for u in users_ws if u in user_clients]
 
-                                await self.update(user_id, bot_id, data)
+                        try:
+                            data["info"]["members"] = len(users_ws)
+                        except KeyError:
+                            pass
 
-                            case "idle":
+                        for u_id in users_ws:
 
-                                text_idle = self.get_lang("idle")
+                            users.add(u_id)
 
-                                data = {
-                                    "assets": {
-                                        "large_image": data.pop("thumb", "app")
-                                    },
-                                    "details": text_idle[0],
-                                }
+                            try:
+                                user = user_clients[u_id]["user"]
+                            except KeyError:
+                                continue
 
-                                if len(text_idle) > 1:
-                                    data['state'] = text_idle[1]
+                            print(f"op: {op} | {user} [{u_id}] | bot: {bot_id}")
 
-                                if public:
-                                    invite = f"https://discord.com/api/oauth2/authorize?client_id={bot_id}&permissions=8&scope=bot%20applications.commands"
+                            match op:
 
-                                    data["buttons"] = [
-                                        {
-                                            "label": self.get_lang("invite"),
-                                            "url": invite
-                                        }
-                                    ]
+                                case "update":
 
-                                await self.update(user_id, bot_id, data)
+                                    await self.update(u_id, bot_id, data)
 
-                            case "close":
+                                case "idle":
 
-                                try:
-                                    users.remove(user_id)
-                                except:
-                                    pass
+                                    text_idle = self.get_lang("idle")
 
-                                await self.update(user_id, bot_id, {})
+                                    data = {
+                                        "assets": {
+                                            "large_image": data.pop("thumb", config["assets"]["app"])
+                                        },
+                                        "details": text_idle[0],
+                                    }
 
-                            case _:
-                                print(f"unknow op: {msg.data}")
+                                    if len(text_idle) > 1:
+                                        data['state'] = text_idle[1]
+
+                                    if public:
+                                        invite = f"https://discord.com/api/oauth2/authorize?client_id={bot_id}&permissions=8&scope=bot%20applications.commands"
+
+                                        data["buttons"] = [
+                                            {
+                                                "label": self.get_lang("invite"),
+                                                "url": invite
+                                            }
+                                        ]
+
+                                    await self.update(u_id, bot_id, data)
+
+                                case "close":
+
+                                    try:
+                                        users.remove(u_id)
+                                    except:
+                                        pass
+
+                                    await self.update(u_id, bot_id, {})
+
+                                case _:
+                                    print(f"unknow op: {msg.data}")
 
 
-            except (websockets.ConnectionClosedError, ConnectionResetError) as e:
+            except (websockets.ConnectionClosedError, ConnectionResetError, websockets.InvalidStatusCode) as e:
                 print(f"Conexão perdida com o servidor: {uri} | Reconectando em {backoff} seg. {repr(e)}")
                 await self.clear_users_presences(users, bots)
                 await asyncio.sleep(backoff)
