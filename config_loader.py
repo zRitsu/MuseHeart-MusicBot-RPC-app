25afc1df14e088ad5a484ace5af580f6bbddbcb6
Alex <74823568+zRitsu@users.noreply.github.com>
2023-01-17 23:54:45 -0300

    oops.

--- config_loader.py
+++ config_loader.py
@@ -20,7 +20,17 @@ def read_config():
         "heartbeat": 30,
         "reconnect_timeout": 7,
         "assets": {
-            "sources": {}
+            "loop": "https://cdn.discordapp.com/emojis/912965656624889916.gif",
+            "loop_queue": "https://i.ibb.co/5Mj4HjT/loop-track.gif",
+            "pause": "https://i.ibb.co/mDBMnH8/pause.png",
+            "stream": "https://i.ibb.co/Qf9BSQb/stream.png",
+            "idle": "https://i.ibb.co/6XS6qLy/music-img.png",
+            "sources": {
+                "deezer": "https://i.ibb.co/zxpBbp8/deezer.png",
+                "soundcloud": "https://i.ibb.co/CV6NB6w/soundcloud.png",
+                "spotify": "https://i.ibb.co/3SWMXj8/spotify.png",
+                "youtube": "https://i.ibb.co/LvX7dQL/yt.png"
+            }
         }
     }
 
@@ -34,25 +44,14 @@ def read_config():
     else:
         with open("./config.json") as f:
 
-            base_config.update(json.load(f))
+            file_config = json.load(f)
 
-            base_config["assets"].update(
-                {
-                    "loop":"https://cdn.discordapp.com/emojis/912965656624889916.gif",
-                    "loop_queue": "https://i.ibb.co/5Mj4HjT/loop-track.gif",
-                    "pause": "https://i.ibb.co/mDBMnH8/pause.png",
-                    "stream": "https://i.ibb.co/Qf9BSQb/stream.png",
-                    "idle": "https://i.ibb.co/6XS6qLy/music-img.png",
-                }
-            )
+            assets = file_config.pop("assets", {})
+            sources = assets.pop("sources", {})
 
-            base_config["assets"]["sources"].update(
-                {
-                    "deezer": "https://i.ibb.co/zxpBbp8/deezer.png",
-                    "soundcloud": "https://i.ibb.co/CV6NB6w/soundcloud.png",
-                    "spotify": "https://i.ibb.co/3SWMXj8/spotify.png",
-                    "youtube": "https://i.ibb.co/LvX7dQL/yt.png"
-                }
-            )
+            base_config.update(file_config)
+
+            base_config["assets"].update(assets)
+            base_config["assets"]["sources"].update(sources)
 
     return base_config
