7f60f70931d4dfcc69af4c7087f1e26badaf4e09
Alex <74823568+zRitsu@users.noreply.github.com>
2023-08-07 22:56:08 -0300

    Use pyinstaller instead of cx-Freeze for exe building.

--- build.bat
+++ build.bat
@@ -1,2 +1,10 @@
-py -3 build.py build
+if not exist venv (
+  py -3 -m venv venv
+  call "venv\scripts\activate"
+  pip install -r requirements.txt
+) else (
+  call "venv\scripts\activate"
+)
+
+pyinstaller -F -i icon.ico -c --noconsole rpc_client.py
 pause
