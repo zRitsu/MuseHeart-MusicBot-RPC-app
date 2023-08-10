f99b1be76cd1e2277be5dbd14e08c698eaa9eb51
Alex <74823568+zRitsu@users.noreply.github.com>
2023-08-09 22:05:36 -0300

    Added start.bat

---
+++ start.bat
@@ -0,0 +1,10 @@
+if not exist venv (
+  py -3 -m venv venv
+  call "venv\scripts\activate"
+  pip install -r requirements.txt
+) else (
+  call "venv\scripts\activate"
+)
+
+python rpc_client.py
+pause
